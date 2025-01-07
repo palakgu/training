68a111f4014602cb436c3d91146a17d15100c6bf
palak gupta <palakofficial21@gmail.com>
2025-01-07 14:47:01 +0530

    second commit

--- welcome.java
+++ welcome.java
@@ -1,5 +1,13 @@
+import java.util.scanner;
 class Hello{
 public static void main(String args[]){
 System.out.println("Hello World");
+System.out.println("")
+Scanner sc = new Scanner(system.in);
+int n = sc.nextInt();
+int m = sc.nextInt();
+int result = n+m;
+System.out.println("result");
+ 
 }
 }
